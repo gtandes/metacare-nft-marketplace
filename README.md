d541f264b4e32166abc3e75b4aaf5e18f1742e68
Greg Thomas Andes <58809832+gtandes@users.noreply.github.com>
2023-01-16 09:49:11 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# metacare-nft-marketplace
+## NFT Marketplace Tutorial
+
+Get started by running:
+
+`npm install`
+`npm run dev`
+
+Please switch to the `blank` branch for the purpose of the tutorial, as the `main` branch already contains the finished product :P
---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import type { NextPage } from "next";
+import {
+  useActiveListings,
+  useAddress,
+  useContract,
+} from "@thirdweb-dev/react";
+import Link from "next/link";
+import NFTCard from "../components/NFTCard";
+
+const Home: NextPage = () => {
+  const address = useAddress();
+
+  const { contract } = useContract(
+    "0xfefe7f5BF815a67e7307e0cEA217189eeA5E1BEB",
+    "marketplace"
+  );
+
+  const { data: nfts, isLoading } = useActiveListings(contract);
+  console.log(nfts);
+
+  if (isLoading)
+    return (
+      <div className={"mb-3 flex w-screen justify-center"}>Loading ...</div>
+    );
+
+  return (
+    <div className={"space-y-4 p-2"}>
+      <div className={"flex space-x-4"}>
+        <div className={"text-2xl font-semibold"}>Active Listings</div>
+        <Link href={`profile/${address}`}>
+          <div className={"cursor-pointer text-2xl font-semibold"}>
+            My Collections
+          </div>
+        </Link>
+      </div>
+
+      <div className={`nft-grid`}>
+        {nfts &&
+          nfts.map((nft) => {
+            return (
+              <Link
+                href={`/assets/${nft.id}`}
+                key={nft.assetContractAddress + nft.id}
+              >
+                <a>
+                  <NFTCard
+                    nft={{
+                      name: nft.asset.name as string,
+                      tokenUri: nft.asset.image as string,
+                      price: nft.buyoutCurrencyValuePerToken?.displayValue,
+                    }}
+                  />
+                </a>
+              </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
